98c470979850fc520035681c27231a056e93eda6
Nicolapps <webmaster@nicolapps.ch>
2016-10-06 21:19:34 +0200

    Ajout des fichiers

---
+++ main.js
@@ -0,0 +1,17 @@
+
+$('.player').stopwatch({
+    format: '{MM}:{ss}'
+}).stopwatch('start').stopwatch('stop')
+
+$('.player').click(function(){
+    $('.player, .pause').removeClass('active')
+    $('.player').stopwatch('stop')
+
+    $(this).addClass('active');
+    $(this).stopwatch('toggle')
+})
+
+$('.pause').click(() => {
+    $('.player').removeClass('active').stopwatch('stop');
+    $(this).addClass('active');
+})
