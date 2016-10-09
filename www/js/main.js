5844c306a62baed2ba6e99f0573197ca0c3f1ac3
Nicolapps <webmaster@nicolapps.ch>
2016-10-09 15:40:06 +0200

    Bouton supprimé

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,5 @@
+var paused = false;
+
 $('.player').stopwatch({
     format: '{MM}:{ss}'
 }).stopwatch('start').stopwatch('stop')
@@ -6,11 +8,24 @@ $('.player').click(function(){
     $('.player, .pause').removeClass('active')
     $('.player').stopwatch('stop')
 
-    $(this).addClass('active');
+    $(this).addClass('active')
     $(this).stopwatch('toggle')
 })
 
 $('.pause').click(function(){
-    $('.player').removeClass('active').stopwatch('stop');
-    $('.pause').addClass('active');
+    if(!paused) {
+        paused = true
+
+        $('.pause').text('✖').addClass('delete')
+
+        $('.player').removeClass('active').stopwatch('stop')
+        $('.pause').addClass('active')
+
+    } else {
+
+        $('.player').stopwatch('reset').text('00:00')
+        paused = false;
+        $('.pause').text('▌▌').removeClass('delete')
+
+    }
 })
